8d30f31b98ee978e45d577427cddf0f2ad1c2c71
marynghiem <marynghiem46@gmail.com>
2022-12-08 22:38:30 -0800

    imported multiple icons from material ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,11 @@ import HomeIcon from "@mui/icons-material/Home";
 import SidebarOption from "./SidebarOption";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const Sidebar = () => {
   return (
@@ -13,6 +18,7 @@ const Sidebar = () => {
       <SidebarOption Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={SearchIcon} text="Explore" />
       <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="" />
     </div>
   );
 };
