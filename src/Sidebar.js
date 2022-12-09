d9bfb45c9781927e799f2565509947b0649b9c66
marynghiem <marynghiem46@gmail.com>
2022-12-08 22:34:32 -0800

    passed props through sidebar option component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,12 +3,16 @@ import "./Sidebar.css";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import HomeIcon from "@mui/icons-material/Home";
 import SidebarOption from "./SidebarOption";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <TwitterIcon />
-      <SidebarOption />
+      <SidebarOption Icon={HomeIcon} text="Home" />
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
     </div>
   );
 };
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./SidebarOption.css";
 
-const SidebarOption = () => {
+const SidebarOption = ({ text, Icon }) => {
   return (
     <div className="sidebar_option">
-      <h1></h1>
+      <Icon />
+      <h2>{text}</h2>
     </div>
   );
 };
