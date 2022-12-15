4887d926cad7bb7957871c10a18173af080a4cce
marynghiem <marynghiem46@gmail.com>
2022-12-14 21:17:42 -0800

    added image inside tweetbox component and added form

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,10 +1,16 @@
+import { Avatar, Button } from "@material-ui/core";
 import React from "react";
 import "./TweetBox.css";
 
 const TweetBox = () => {
   return (
-    <div>
-      <h1></h1>
+    <div className="tweet_box">
+      <form>
+        <div className="tweet_box_input">
+          <Avatar src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fmobile.twitter.com%2Fmary_nghiem&psig=AOvVaw0DqWJ-6xMS70sqIFxWOl75&ust=1671167723408000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJja_4Xv-vsCFQAAAAAdAAAAABAE" />
+        </div>
+        <Button>Tweet</Button>
+      </form>
     </div>
   );
 };
