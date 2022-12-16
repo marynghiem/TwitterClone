c02991bca5479e7e588042145c86b4a489715d63
marynghiem <marynghiem46@gmail.com>
2022-12-15 20:42:23 -0800

    styled tweetbox component

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Feed.css";
+import TweetBox from "./TweetBox";
 
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feed_header">
         <h2>Home</h2>
+        <TweetBox />
       </div>
     </div>
   );
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -8,6 +8,8 @@ const TweetBox = () => {
       <form>
         <div className="tweet_box_input">
           <Avatar src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fmobile.twitter.com%2Fmary_nghiem&psig=AOvVaw0DqWJ-6xMS70sqIFxWOl75&ust=1671167723408000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJja_4Xv-vsCFQAAAAAdAAAAABAE" />
+          <input placeholder="What's happening?" type="text" />
+          <input placeholder="Enter image URL" type="text" />
         </div>
         <Button>Tweet</Button>
       </form>
