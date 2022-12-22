7c2278ca8ff46a06e45cd3d3870bbb01cbe6d387
marynghiem <marynghiem46@gmail.com>
2022-12-21 20:12:57 -0800

    installed twitter embed and created widget input container with inputs

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Widgets.css";
-
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 const Widgets = () => {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets_input">
+        <SearchIcon className="widgets_search_icon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets_widget_container">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+      </div>
     </div>
   );
 };
