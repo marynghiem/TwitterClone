5f186480e750ecee3dfd42029cdd9993966df1e3
marynghiem <marynghiem46@gmail.com>
2022-12-27 15:30:54 -0800

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAwZTOj6WukyiOa5lVRV7f-aLH0KS4l3ZU",
+  authDomain: "twitter-clone-9c754.firebaseapp.com",
+  projectId: "twitter-clone-9c754",
+  storageBucket: "twitter-clone-9c754.appspot.com",
+  messagingSenderId: "615471113048",
+  appId: "1:615471113048:web:bb5aa9d3619c6eb15ebb2d",
+  measurementId: "G-38ZJLJV1BP",
+};
